9b031cede1b1bb7f5d3fd7a5eca0f2b8a05626af
25019林后辰 <43640545+mqlingithub@users.noreply.github.com>
2018-11-11 14:40:32 +0800

    Create 25019function.cpp

---
+++ 25019function.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void length(int);
+int main() {
+  int height;
+  cout << "請輸入三角形高度(高度勿大於8或小於1):";
+  cin >> height;
+  if ( height < 9 && height > 0 ) {
+    for ( int a = 0 ;a < height ; a++ ) {
+      for (int b = height ; b > a ; b-- ) {
+        cout << " ";
+      }
+      length (a);
+      cout << "  ";
+      length (a);
+      cout << "\n";
+    }
+    cout << "高度:" << height << "\n";
+  }
+  else {
+    cout << "高度大於8或小於1。\n";
+  }
+}
+void length (int length) {
+  for ( int b = 0; b <= length; b++ ) {
+    cout << "#";
+  }
+}
